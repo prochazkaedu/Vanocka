1eb538f494ae895210d30db6eef94b53f75450b5
Michal Procházka <michal.prochazka@tul.cz>
2023-11-21 12:43:15 +0100

    Forget the primary/secondary axis complexity, we'll just rotate the world instead

--- src/main/java/eu/prochazkaml/vanocka/RayCaster.java
+++ src/main/java/eu/prochazkaml/vanocka/RayCaster.java
@@ -33,106 +33,84 @@ public class RayCaster {
 		return map.map[y][x];
 	}
 
-	private char getBlockRelative(int x, int y, boolean swapXY, boolean mainAxisNegative) {
-		if(!swapXY) {
-			return getBlock(x, y);
-		} else {
-			return getBlock(y, x);
-		}
+	private enum WorldRotation {
+		DEG_0,
+		DEG_90,
+		DEG_180,
+		DEG_270
+	};
+
+	private char getBlockRelative(int x, int y, WorldRotation rot) {
+		return getBlock(x, y);
 	}
 
-	private void calculateRayDistance(RayCasterResult retval, double dx, double dy, boolean swapXY, boolean mainAxisNegative) {
-		double posPrimary = 0, posSecondary = 0, oldPosPrimary, oldPosSecondary;
-		double toTravelPrimary = 0, toTravelSecondary = 0;
-		double deltaPrimary = 0, deltaSecondary = 0;
+	private void calculateRayDistance(RayCasterResult retval, double deltaX, double deltaY, WorldRotation rot) {
+		double posX = 0, posY = 0, oldPosX, oldPosY;
+		double toTravelX = 0, toTravelY = 0;
 
 		retval.wallColor = 0xFFFFFF;
 		retval.distance = 0;
 
-		if(debugOutput) System.err.printf("- Incrementing X by %f, Y by %f.\n", dx, dy);
+		if(debugOutput) System.err.printf("- Incrementing X by %f, Y by %f.\n", deltaX, deltaY);
 
-		if(!swapXY) {
-			if(debugOutput) System.err.printf(" - RAY GOING %s: X = primary, Y = secondary\n", mainAxisNegative ? "LEFT" : "RIGHT");
+		posX = playerX;
+		posY = playerY;
 
-			posPrimary = playerX;
-			posSecondary = playerY;
+		toTravelX = Math.floor(playerX + 1) - playerX;
+		toTravelY = deltaY * toTravelX / deltaX;
 
-			deltaPrimary = dx;
-			deltaSecondary = dy;
-
-			toTravelPrimary = Math.floor(playerX + 1) - playerX;
-			toTravelSecondary = deltaSecondary * toTravelPrimary / deltaPrimary;
-		} else {
-			if(debugOutput) System.err.printf(" - RAY GOING %s: X = secondary, Y = primary\n", mainAxisNegative ? "UP" : "DOWN");
-
-			posPrimary = playerY;
-			posSecondary = playerX;
-
-			deltaPrimary = dy;
-			deltaSecondary = dx;
-
-			toTravelPrimary = Math.floor(playerY + 1) - playerY;
-			toTravelSecondary = deltaSecondary * toTravelPrimary / deltaPrimary;
-		}
-
-		if(debugOutput) System.err.printf(" - First increment primary by %f, secondary by %f.\n", toTravelPrimary, toTravelSecondary);
+		if(debugOutput) System.err.printf(" - First increment X by %f, Y by %f.\n", toTravelX, toTravelY);
 
 		while(true) {
-			oldPosPrimary = posPrimary;
-			oldPosSecondary = posSecondary;
+			oldPosX = posX;
+			oldPosY = posY;
 
-			posPrimary += toTravelPrimary;
-			posSecondary += toTravelSecondary;
+			posX += toTravelX;
+			posY += toTravelY;
 
 			if(debugOutput) System.err.printf("- %f/%f + %f/%f = %f/%f\n",
-				oldPosPrimary, oldPosSecondary,
-				toTravelPrimary, toTravelSecondary,
-				posPrimary, posSecondary);
+				oldPosX, oldPosY,
+				toTravelX, toTravelY,
+				posX, posY);
 
-			if((int)oldPosSecondary != (int)posSecondary) {
-				if(debugOutput) System.err.printf(" - CROSSED SECONDARY BOUNDARY (%d/%d)\n", (int)oldPosPrimary, (int)posSecondary);
+			if((int)oldPosY != (int)posY) {
+				if(debugOutput) System.err.printf(" - CROSSED HORIZONTAL BOUNDARY (%d/%d)\n", (int)oldPosX, (int)posY);
 
 				double ratio = 0;
 
-				if(toTravelSecondary > 0) {
-					ratio = (((int)posSecondary) - oldPosSecondary) / toTravelSecondary;
+				if(toTravelY > 0) {
+					ratio = (((int)posY) - oldPosY) / toTravelY;
 				} else {
-					ratio = (((int)oldPosSecondary) - oldPosSecondary) / toTravelSecondary;
+					ratio = (((int)oldPosY) - oldPosY) / toTravelY;
 				}
 
-				if(debugOutput) System.err.printf("   - ratio = %f (%f / %d)\n", ratio, oldPosSecondary, (int)(oldPosSecondary));
+				if(debugOutput) System.err.printf("   - ratio = %f (%f / %d)\n", ratio, oldPosY, (int)(oldPosY));
 
-				if(getBlockRelative((int)oldPosPrimary, (int)posSecondary, swapXY, mainAxisNegative) != '.') {
-					if(debugOutput) System.err.printf(" - Reached secondary wall at %f/%f (block %d/%d)\n",
-						posPrimary, posSecondary, (int)oldPosPrimary, (int)posSecondary);
+				if(getBlockRelative((int)oldPosX, (int)posY, rot) != '.') {
+					if(debugOutput) System.err.printf(" - Reached horizontal wall at %f/%f (block %d/%d)\n",
+						posX, posY, (int)oldPosX, (int)posY);
 					
-					posPrimary = oldPosPrimary + toTravelPrimary * ratio;
-					posSecondary = oldPosSecondary + toTravelSecondary * ratio;
+					posX = oldPosX + toTravelX * ratio;
+					posY = oldPosY + toTravelY * ratio;
 					
-					if(!swapXY) retval.wallColor = 0xE0E0E0;
-					
+					if(rot == WorldRotation.DEG_0 || rot == WorldRotation.DEG_180) retval.wallColor = 0xE0E0E0;
 					break;
 				}
 			}
 
-			if(getBlockRelative((int)posPrimary, (int)posSecondary, swapXY, mainAxisNegative) != '.') {
-				if(debugOutput) System.err.printf(" - Reached primary wall at %f/%f (block %d/%d)\n",
-					posPrimary, posSecondary, (int)posPrimary, (int)posSecondary);
+			if(getBlockRelative((int)posX, (int)posY, rot) != '.') {
+				if(debugOutput) System.err.printf(" - Reached vertical wall at %f/%f (block %d/%d)\n",
+					posX, posY, (int)posX, (int)posY);
 
-				if(swapXY) retval.wallColor = 0xE0E0E0;
-
+				if(rot == WorldRotation.DEG_90 || rot == WorldRotation.DEG_270) retval.wallColor = 0xE0E0E0;
 				break;
 			}
 
-			toTravelPrimary = deltaPrimary;
-			toTravelSecondary = deltaSecondary;
+			toTravelX = deltaX;
+			toTravelY = deltaY;
 		}
 
-		if(!swapXY) {
-			retval.distance = Math.hypot(playerX - posPrimary, playerY - posSecondary);
-		} else {
-			retval.distance = Math.hypot(playerX - posSecondary, playerY - posPrimary);
-		}
+		retval.distance = Math.hypot(playerX - posX, playerY - posY);
 
 		if(debugOutput) System.err.printf(" *** RESULT DISTANCE = %f, COLOR = 0x%06X\n", retval.distance, retval.wallColor);
 	}
@@ -145,43 +123,18 @@ public class RayCaster {
 			double relangle = playerFOV * (double)x / (double)vw - playerFOV / 2;
 			double angle = playerAngle + relangle;
 
-			if(angle == 0) angle += Double.MIN_VALUE; // Prevents NaN
-			double tmptan = Math.tan(angle);
-
-			double horizUnitDeltaY = tmptan * Math.signum(Math.cos(angle));
-			double vertUnitDeltaX = 1 / tmptan * Math.signum(Math.sin(angle));
-
-			if(debugOutput) System.err.printf("%d: %f %f/%f\n", x, angle, vertUnitDeltaX, horizUnitDeltaY);
-
-			if(Math.abs(horizUnitDeltaY) < Math.abs(vertUnitDeltaX)) {
-				// The fired ray will mainly go left/right
-
-				double dx = Math.signum(vertUnitDeltaX), dy = horizUnitDeltaY;
-
-				// dx = ±1
-				// dy = anywhere from -1 to 1
-
-				// TODO - finish the other quadrants
-
-				if(dx > 0) { // Right
-					calculateRayDistance(rayResult, dx, dy, false, false);
-				} else if(dx < 0) { // Left
-					//calculateRayDistance(rayResult, dx, dy, false, true);
-				}
-			} else {
-				// The fired ray will mainly go up/down
-
-				double dx = vertUnitDeltaX, dy = Math.signum(horizUnitDeltaY);
-
-				// dx = anywhere from -1 to 1
-				// dy = ±1
-
-				if(dy > 0) { // Down
-					calculateRayDistance(rayResult, dx, dy, true, false);
-				} else if(dy < 0) {
-					//calculateRayDistance(rayResult, dx, dy, true, true);
-				}
+			if(angle <= -Math.PI / 4 || angle >= Math.PI / 4) {
+				drawColumn(x, 0, 0x000000, 0x000000, 0xFF0000);
+				continue;
 			}
+
+			double horizUnitDeltaY = Math.tan(angle);
+
+			if(debugOutput) System.err.printf("%d: angle %f, Y delta %f\n", x, angle, horizUnitDeltaY);
+
+			// TODO - finish the other quadrants
+
+			calculateRayDistance(rayResult, 1, horizUnitDeltaY, WorldRotation.DEG_0);
 
 			// Perform fish-eye correction
 
