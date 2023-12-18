07f96cde527ab987feb1fe8b2f71e95ab98d4155
Michal Procházka <michal.prochazka@tul.cz>
2023-12-18 07:20:50 +0100

    The assignment said that we need to accept at least 2 parameters

--- src/main/java/eu/prochazkaml/vanocka/Main.java
+++ src/main/java/eu/prochazkaml/vanocka/Main.java
@@ -5,9 +5,48 @@ import eu.prochazkaml.vanocka.scenes.StoryIntro;
 import eu.prochazkaml.vanocka.scenes.Maze;
 import eu.prochazkaml.vanocka.scenes.StoryOutro;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		FrameBuffer fb = new FrameBuffer(80, 24, 20);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Vítejte v mé vánoční hře!\n\n");
+		System.out.print("Tato hra potřebuje vědět, jak velký terminál máte. Minimální velikost je 80x24 znaků (= 80x48 pixelů).\n\n");
+		
+		int width = 80, height = 24;
+
+		System.out.print("Zadejte šířku terminálu: ");
+
+		try {
+			width = sc.nextInt();
+		} catch(Exception e) {
+			System.out.println("Neplatný vstup!");
+			System.exit(1);
+		}
+
+		if(width < 80) {
+			System.out.println("Zadaná šířka je příliš malá, zvětšuji na 80 znaků.");
+			width = 80;
+		}
+
+		System.out.print("Zadejte výšku terminálu: ");
+
+		try {
+			height = sc.nextInt();
+		} catch(Exception e) {
+			System.out.println("Neplatný vstup!");
+			System.exit(1);
+		}
+
+		if(height < 24) {
+			System.out.println("Zadaná výška je příliš malá, zvětšuji na 24 znaků.");
+			height = 24;
+		}
+
+		sc.close();
+
+		FrameBuffer fb = new FrameBuffer(width, height, 20);
 
 		// ========== FOR TESTING PURPOSES ==========
 
